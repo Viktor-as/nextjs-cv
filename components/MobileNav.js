f968906b86c82a4a39e7051ef788adf9629ae0b8
unknown <simkevicius.viktoras@gmail.com>
2022-10-14 01:05:17 +0300

    mobile response added

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -25,27 +25,43 @@ export default function Header(props) {
   function changeLanguageToEn() {
     router.push({ pathname, query }, asPath, { locale: "en" });
   }
+
+  function unCheck() {
+    document.getElementById("mobMenuCheckbox").checked = false;
+  }
   return (
     <>
       <nav role="navigation" className={styles.mobNavContainer}>
         <div id={styles.menuToggle}>
-          <input type="checkbox" id={styles.mobMenuCheckbox} />
+          <input type="checkbox" id="mobMenuCheckbox" />
           <span></span>
           <span></span>
           <span></span>
           <div className={styles.languageContainer}>
             <p>{t.language}</p>
-            <div className={styles.languageImg} onClick={changeLanguageToEn}>
+            <div
+              className={styles.languageImg}
+              onClick={() => {
+                changeLanguageToEn();
+                unCheck();
+              }}
+            >
               <Image src={enFlag} layout="fill" objectFit="contain" />
             </div>
-            <div className={styles.languageImg} onClick={changeLanguageToLt}>
+            <div
+              className={styles.languageImg}
+              onClick={() => {
+                changeLanguageToLt();
+                unCheck();
+              }}
+            >
               <Image src={ltFlag} layout="fill" objectFit="contain" />
             </div>
           </div>
           <ul id={styles.menu}>
             <li>
               <Link href="/#home">
-                <a>
+                <a onClick={unCheck}>
                   <div className={styles.svg}>
                     <Image src={homeIcon} />
                   </div>
@@ -55,7 +71,7 @@ export default function Header(props) {
             </li>
             <li>
               <Link href="/#portfolio">
-                <a>
+                <a onClick={unCheck}>
                   <Image className={styles.svg} src={portfolioIcon} />
                   <p>{t.portfolio}</p>
                 </a>
@@ -63,7 +79,7 @@ export default function Header(props) {
             </li>
             <li>
               <Link href="/#experience">
-                <a>
+                <a onClick={unCheck}>
                   <Image className={styles.svg} src={experienceIcon} />
                   <p>{t.experience}</p>
                 </a>
@@ -71,7 +87,7 @@ export default function Header(props) {
             </li>
             <li>
               <Link href="/#about">
-                <a>
+                <a onClick={unCheck}>
                   <Image className={styles.svg} src={aboutIcon} />
                   <p>{t.about_me}</p>
                 </a>
@@ -79,7 +95,7 @@ export default function Header(props) {
             </li>
             <li>
               <Link href="/#contact">
-                <a>
+                <a onClick={unCheck}>
                   <Image className={styles.svg} src={contactIcon} />
                   <p>{t.contacts}</p>
                 </a>
